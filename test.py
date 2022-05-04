444390ac864b8d2c1b75940ac611541379cab3af
QuarterSalt <104917017+QuarterSalt@users.noreply.github.com>
2022-05-04 11:29:37 -0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,10 @@
+
+class Myclass:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+new_person = Myclass("Leif","15")
+
+print(new_person.age)
+print(new_person.name)
